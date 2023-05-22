60f0d469b1652da418c856d7785ae30b4cab1961
adex-hub <adeolaweb@gmail.com>
2023-05-22 20:44:57 +0100

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 20;
+export const RES_PER_PAGE = 10;
+export const KEY = 'df800bd1-7f17-4ced-bb97-e71ca38d4dde';
+export const MODAL_CLOSE_SEC = 2.5;
